e30a8b7a390bfde4c1acf6c6b6f06799ef262e08
Kyle Mitchell <kyle@kemitchell.com>
2015-12-24 21:13:28 -0800

    Detect long headings

--- README.md
+++ README.md
@@ -35,3 +35,22 @@ assert(suspicious({ content: [ { definition: term } ] }))
 
 assert(suspicious({ content: [ { use: term } ] }))
 ```
+
+# Long Headings
+
+```javascript
+var heading = (
+  'This is a really long heading ' +
+  ' that just keeps going' +
+  ' and going' +
+  ' and going' )
+
+assert(
+  suspicious({ content: [ { reference: heading } ] }))
+
+assert(
+  suspicious({
+    content: [
+      { heading: heading,
+        form: { content: [ 'A' ] } } ] }))
+```
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 module.exports = suspicious
 
 var predicate = require('commonform-predicate')
+
 var MAX_DEPTH = 9
 var MAX_TERM_LENGTH = 50
+var MAX_HEADING_LENGTH = 50
 
 function suspicious(form) {
   return recurse(form, 0) }
@@ -16,12 +18,20 @@ function recurse(form, depth) {
     for (index = 0; index < length; index++) {
       element = form.content[index]
       if (predicate.child(element)) {
-        if (recurse(element.form, ( depth + 1 ))) {
+        var longHeading = (
+          'heading' in element &&
+          element.heading.length > MAX_HEADING_LENGTH )
+        if (longHeading) {
+          return true }
+        else if (recurse(element.form, ( depth + 1 ))) {
           return true } }
       else if (predicate.definition(element)) {
         if (element.definition.length > MAX_TERM_LENGTH) {
           return true } }
       else if (predicate.use(element)) {
         if (element.use.length > MAX_TERM_LENGTH) {
+          return true } }
+      else if (predicate.reference(element)) {
+        if (element.reference.length > MAX_HEADING_LENGTH) {
           return true } } }
     return false } }
