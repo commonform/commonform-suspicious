c180392b6e6d3a608801253282396896636e3818
Kyle Mitchell <kyle@kemitchell.com>
2015-12-24 21:27:54 -0800

    Refactor

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
 module.exports = suspicious
 
-var predicate = require('commonform-predicate')
+var is = require('commonform-predicate')
 
 var MAX_DEPTH = 9
-var MAX_TERM_LENGTH = 50
+var MAX_CONTENT_LENGTH = 100
 var MAX_HEADING_LENGTH = 50
-var MAX_CONTENT_LENGTH = 50
+var MAX_TERM_LENGTH = 50
 
-function suspicious(form) {
-  return recurse(form, 0) }
-
-function recurse(form, depth) {
+function suspicious(form, depth) {
+  var index, length, element
+  if (depth === undefined) {
+    depth = 0 }
   if (depth > MAX_DEPTH) {
     return true }
   else {
-    var index, length, element
     length = form.content.length
     if (length > MAX_CONTENT_LENGTH) {
       return true }
     else {
       for (index = 0; index < length; index++) {
         element = form.content[index]
-        if (predicate.child(element)) {
-          var longHeading = (
-            'heading' in element &&
-            element.heading.length > MAX_HEADING_LENGTH )
-          if (longHeading) {
-            return true }
-          else if (recurse(element.form, ( depth + 1 ))) {
-            return true } }
-        else if (predicate.definition(element)) {
-          if (element.definition.length > MAX_TERM_LENGTH) {
-            return true } }
-        else if (predicate.use(element)) {
-          if (element.use.length > MAX_TERM_LENGTH) {
-            return true } }
-        else if (predicate.reference(element)) {
-          if (element.reference.length > MAX_HEADING_LENGTH) {
-            return true } } }
+        if (
+          ( is.child(element) &&
+            ( ( ( 'heading' in element ) &&
+                ( element.heading.length > MAX_HEADING_LENGTH ) ) ||
+              suspicious(element.form, ( depth + 1 )) ) ) ||
+          ( is.definition(element) &&
+            ( element.definition.length > MAX_TERM_LENGTH ) ) ||
+          ( is.use(element) &&
+            ( element.use.length > MAX_TERM_LENGTH ) ) ||
+          ( is.reference(element) &&
+            ( element.reference.length > MAX_HEADING_LENGTH ) ) )
+            { return true } }
       return false } } }
